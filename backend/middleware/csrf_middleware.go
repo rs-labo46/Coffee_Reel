6e82112a9fe0012fe4916621fd5f510796f5dc51
rs-labo46 <r.s.labo46@gmail.com>
2026-07-20 23:24:17 +0900

    csrf middlewareを追加

---
+++ backend/middleware/csrf_middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"coffee-reel/usecase"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	csrfCookieName = "csrf_token"
+	csrfHeaderName = "X-CSRF-Token"
+)
+
+type CSRFMiddleware struct {
+	tokens usecase.ITokenService
+}
+
+func NewCSRFMiddleware(tokens usecase.ITokenService) *CSRFMiddleware {
+	return &CSRFMiddleware{tokens}
+}
+
+// CSRF CookieとX-CSRF-Token Headerが一致する場合だけ後続処理を実行する
+func (m *CSRFMiddleware) Validate(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie(csrfCookieName)
+		if err != nil {
+			return writeMiddlewareError(c, http.StatusForbidden, "csrf_invalid", "CSRFトークンが無効です")
+		}
+		headerToken := c.Request().Header.Get(csrfCookieName)
+
+		if err := m.tokens.CompareCSRFToken(cookie.Value, headerToken); err != nil {
+			return writeMiddlewareError(c, http.StatusForbidden, "csrf_invalid", "CSRFトークンが無効です")
+		}
+		return next(c)
+	}
+}
